38d737fbe3dec9680ac2361547f17bf0918de446
Zach Antunes <ztunes4@gmail.com>
2023-06-14 13:56:37 -0700

    got the search API working, getting errors from the regular API

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,10 +2,33 @@
 
 // Search function calls the weather API
 
-var searchApi = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=9efcb564494f2e20b99f73f7925ec30e`
-
-var searchInput = document.getElementById('search-box').value;
-
 document.getElementById('search-button').addEventListener('click', function() {
-    console.log("Search Button Clicked!")
+    var searchInput = document.getElementById('search-box').value;
+    console.log(searchInput);
+
+    var searchApi = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=9efcb564494f2e20b99f73f7925ec30e`;
+
+    var lat, lon;
+
+    fetch(searchApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+        });
+
+        var weatherApi = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9efcb564494f2e20b99f73f7925ec30e`;
+
+    fetch (weatherApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
 });
