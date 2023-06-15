d584899e3766448375e7dee28a6cc58247524fd1
Zach Antunes <ztunes4@gmail.com>
2023-06-15 11:34:43 -0700

    got both APIs working

--- assets/javascript.js
+++ assets/javascript.js
@@ -6,9 +6,9 @@ document.getElementById('search-button').addEventListener('click', function() {
     var searchInput = document.getElementById('search-box').value;
     console.log(searchInput);
 
-    var searchApi = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=9efcb564494f2e20b99f73f7925ec30e`;
+    document.getElementById("city-header").textContent = searchInput;
 
-    var lat, lon;
+    var searchApi = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=9efcb564494f2e20b99f73f7925ec30e`;
 
     fetch(searchApi)
         .then(function (response) {
@@ -19,16 +19,16 @@ document.getElementById('search-button').addEventListener('click', function() {
             var lat = data[0].lat;
             var lon = data[0].lon;
             console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+        
+
+        var weatherApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9efcb564494f2e20b99f73f7925ec30e`;
+
+        fetch (weatherApi)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
         });
-
-        var weatherApi = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9efcb564494f2e20b99f73f7925ec30e`;
-
-    fetch (weatherApi)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-
 });
